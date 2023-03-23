ba07bee5d6c4bf5c9df2f684a5efe6d33323a586
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 10:21:09 -0600

    Empezando a trabajar con Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
+// Estas constantes no cambian, se generan antes de la ejecucion
+// Declarar en tiempo de compilacion
+const val PI = 3.1415;
+
 
 // funcion main -> punto de entrada de la app
 fun main(args: Array<String>) {
-    println("Hello World!")
+//    variables creadas en tiempo de ejecucion
+//    variables de lectura y escritura
+    var dinero = 1000
+    dinero -= 50
+    println(dinero)
+
+//    Variables de solo lectura
+    val nombre = "Jesus Alexis"
+    println(nombre)
+    println(PI)
 }
