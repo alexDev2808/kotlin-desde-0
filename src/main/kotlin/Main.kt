4aa65b45310f210c6cf9f5a61a6b01e8b27b9eee
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 11:06:47 -0600

    Kotlin y sus tipos de dato

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,4 +15,29 @@ fun main(args: Array<String>) {
     val nombre = "Jesus Alexis"
     println(nombre)
     println(PI)
+
+// Tipos de datos
+    val boolean = true; //Boolenas true o false
+    val numeroLargo : Long = 1000000000 // o 10000L
+    val numDecimal = 4524.2 // Numeros decimales por defecto son Doubles
+    val numFlotante = 104.4f // Flotantes van con una f al final
+
+//    Al crear una variable cada tipo de dato tiene sus propios metodos y propiedades
+//    En Kotlin todo es un objeto!
+
+    val primer_valor = 50
+    val segundo_valor = 25
+    val tercer_valor = primer_valor - segundo_valor
+    println(tercer_valor)
+
+    val apellidoP = "Tenorio"
+    val apellidoM = "Hernandez"
+
+//  Template String / interpolacion
+    val apellidos = "Mis apellidos son: $apellidoP $apellidoM"
+    println(apellidos)
+//    En los templates se pueden desarrollar operaciones van en ${}
+    println("La suma del primer y segundo valor es: ${primer_valor + segundo_valor} MXN")
+
+
 }
