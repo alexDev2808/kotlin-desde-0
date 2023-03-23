d4b266c733effab091eda71c73a19da205e4dafa
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-23 12:10:32 -0600

    Estructuras de control: When

--- src/main/kotlin/estructuras_control.kt
+++ src/main/kotlin/estructuras_control.kt
@@ -8,5 +8,33 @@ fun main() {
     val mensaje = if(nombre.isEmpty()) "El nombre estaba vacio" else if (nombre.length > 5) "Tu nombre es largo" else "Tu nombre es corto"
     println(mensaje)
 
+//    when
+
+    val nombreColor = "Carmesi"
+
+    when (nombreColor) {
+        "Amarillo" -> println("El amarillo es el color de la alegria")
+        "Rojo", "Carmesi" -> println("Roja es la sangre jaja")
+        else -> println("No existe ese color")
+    }
+
+    val codigo = 200
+
+    when (codigo) {
+        in 200..209 -> println("Todo ha ido bien")
+        in 400..499 -> println("Not found")
+        else -> println("Codigo desconocido")
+    }
+
+// Tambien se puede utilizar sin parametro y usarlo como if - else
+    val talla_zapatos = 28
+    val disponible = when {
+        talla_zapatos > 27 -> "En existencia"
+        else -> "Zapatos disponbles solo en tallas 27 y 28"
+    }
+
+    println(disponible)
+
+
 
 }
